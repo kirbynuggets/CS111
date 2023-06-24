ac07d2c1a5fa420ea1d01c50e99bb9e6d0a0903f
kirbynuggets <121129315+kirbynuggets@users.noreply.github.com>
2023-06-25 01:35:04 +0530

    1.c

--- assignment_2/1.c
+++ assignment_2/1.c
@@ -1,93 +1,95 @@
 #include <stdio.h>
 
-#define MAX_ROWS 100
-#define MAX_COLS 100
+int subjectMarks(int * ptr) {
+  int lowest = 0, highest = 100, noOfStud = 50;
 
-int matrix[MAX_ROWS][MAX_COLS];
+  for (int i = 0; i < noOfStud; i++) {
+    int num = (rand() % (highest - lowest + 1)) + lowest;
+    *(ptr + i) = num;
+    //  printf("%d ",*(ptr+i));
+  }
+  printf("\n");
+}
 
-int main() {
-    int num_rows, num_cols;
-    printf("Enter number of rows and columns: ");
-    scanf("%d %d", &num_rows, &num_cols);
+void maxMarks(int * ptr) {
+  int max_marks = * ptr;
+  for (int i = 0; i < 50; i++) {
+    if (max_marks < * (ptr + i))
+      max_marks = * (ptr + i);
+  }
+  printf("Max marks in Hindi are- %d \n", max_marks);
+}
+void minMarks(int * ptr) {
+  int min_marks = * ptr;
+  for (int i = 0; i < 50; i++) {
+    if (min_marks > * (ptr + i))
+      min_marks = * (ptr + i);
+  }
+  printf("Min marks in English are- %d \n", min_marks);
+}
 
-    // Read matrix elements from user
-    printf("Enter matrix elements:\n");
-    for (int i = 0; i < num_rows; i++) {
-        for (int j = 0; j < num_cols; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
+void mathGradeA(int * mathMarks) {
+  int count_A = 0;
+  for (int i = 0; i < 50; i++) {
+    if ( * (mathMarks + i) >= 90) {
 
-    // Print matrix
-    printf("The matrix is:\n");
-    for (int i = 0; i < num_rows; i++) {
-        for (int j = 0; j < num_cols; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
+      count_A++;
     }
+  }
+  printf("Number of As in mathematics %d \n", count_A);
+}
 
-    // Read input number
-    int num;
-    printf("Enter a number to find neighbors: ");
-    scanf("%d", &num);
+void count_students_failed(int * mathMarks, int * engMarks, int * sciMarks, int * hindiMarks) {
+  int count = 0;
+  for (int i = 0; i < 50; i++) {
+    if (( * (mathMarks + i) <= 32) && ( * (engMarks + i) <= 32) && ( * (sciMarks + i) <= 32) && ( * (hindiMarks + i) <= 32))
+      count++;
+  }
+  printf("Number of students who failed - %d \n", count);
+}
+double sumAvg = 0;;
+void subjectWiseMarks(int * marksPerSub) {
+  int marksSum = 0;
+  for (int i = 0; i < 50; i++) {
+    marksSum += * (marksPerSub + i);
+  }
 
-    // Find location of number in matrix
-    int row = -1, col = -1;
-    for (int i = 0; i < num_rows; i++) {
-        for (int j = 0; j < num_cols; j++) {
-            if (matrix[i][j] == num) {
-                row = i;
-                col = j;
-                break;
-            }
-        }
-        if (row != -1 && col != -1) {
-            break;
-        }
-    }
+  double subwiseClassAvg = marksSum / 50.0;
+  sumAvg += subwiseClassAvg;
+  printf("%f \n", subwiseClassAvg);
+}
 
-    // Print neighbors of number in sorted order
-    printf("Neighbors of %d are: ", num);
-    int neighbors[8], num_neighbors = 0;
-    if (row > 0 && col > 0) {
-        neighbors[num_neighbors++] = matrix[row-1][col-1];
-    }
-    if (row > 0) {
-        neighbors[num_neighbors++] = matrix[row-1][col];
-    }
-    if (row > 0 && col < num_cols-1) {
-        neighbors[num_neighbors++] = matrix[row-1][col+1];
-    }
-    if (col > 0) {
-        neighbors[num_neighbors++] = matrix[row][col-1];
-    }
-    if (col < num_cols-1) {
-        neighbors[num_neighbors++] = matrix[row][col+1];
-    }
-    if (row < num_rows-1 && col > 0) {
-        neighbors[num_neighbors++] = matrix[row+1][col-1];
-    }
-    if (row < num_rows-1) {
-        neighbors[num_neighbors++] = matrix[row+1][col];
-    }
-    if (row < num_rows-1 && col < num_cols-1) {
-        neighbors[num_neighbors++] = matrix[row+1][col+1];
-    }
-    // Sort neighbors using bubble sort
-    for (int i = 0; i < num_neighbors-1; i++) {
-        for (int j = 0; j < num_neighbors-i-1; j++) {
-            if (neighbors[j] > neighbors[j+1]) {
-                int temp = neighbors[j];
-                neighbors[j] = neighbors[j+1];
-                neighbors[j+1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < num_neighbors; i++) {
-        printf("%d ", neighbors[i]);
-    }
-    printf("\n");
+void studentwisePercentage(int * mathMarks, int * engMarks, int * sciMarks, int * hindiMarks) {
+  int totalMarks = 0;
+  for (int i = 0; i < 50; i++) {
+    int totalMarks = * (mathMarks + i) + * (engMarks + i) + * (sciMarks + i) + * (hindiMarks + i);
+    double percentage = (totalMarks * 100) / 400.0;
+    printf("percentage of %dth student is %f \n", (i + 1), percentage);
+  }
+}
 
-    return 0;
+void main() {
+
+  int * mathMarks = calloc(50, sizeof(int));
+  subjectMarks(mathMarks);
+  int * engMarks = calloc(50, sizeof(int));
+  subjectMarks(engMarks);
+  int * sciMarks = calloc(50, sizeof(int));
+  subjectMarks(sciMarks);
+  int * hindiMarks = calloc(50, sizeof(int));
+  subjectMarks(hindiMarks);
+  mathGradeA(mathMarks);
+  maxMarks(engMarks);
+  minMarks(hindiMarks);
+  count_students_failed(mathMarks, engMarks, sciMarks, hindiMarks);
+  printf("Avg of Math ");
+  subjectWiseMarks(mathMarks);
+  printf("Avg of English  ");
+  subjectWiseMarks(engMarks);
+  printf("Avg of Hindi  ");
+  subjectWiseMarks(hindiMarks);
+  printf("science average");
+  subjectWiseMarks(sciMarks);
+  printf("Class average %f \n", sumAvg / 4.00);
+  studentwisePercentage(mathMarks, engMarks, sciMarks, hindiMarks);
 }
